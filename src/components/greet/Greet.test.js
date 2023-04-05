e6d8beb0abc8e1a69c152866868f14daf29d2aec
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-04-05 07:44:32 +0530

    learned more about test APIs

--- src/components/greet/Greet.test.js
+++ src/components/greet/Greet.test.js
@@ -1,13 +1,16 @@
 import Greet from "./Greet";
-const { render, screen } = require("@testing-library/react");
-test("Greet renders correctly", () => {
-	render(<Greet />);
-	const textElement = screen.getByText(/greetings/i);
-	expect(textElement).toBeInTheDocument();
-});
 
-test("Greet renders With a name", () => {
-	render(<Greet name='Bhanu' />);
-	const textElement = screen.getByText(/greetings bhanu/i);
-	expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+	const { render, screen } = require("@testing-library/react");
+	test("Renders correctly", () => {
+		render(<Greet />);
+		const textElement = screen.getByText(/greetings/i);
+		expect(textElement).toBeInTheDocument();
+	});
+
+	test("Renders With a name", () => {
+		render(<Greet name='Bhanu' />);
+		const textElement = screen.getByText(/greetings bhanu/i);
+		expect(textElement).toBeInTheDocument();
+	});
 });
